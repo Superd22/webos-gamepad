f9995b7e71c05e2ddf64652e3b2800059d8ab2df
David FAIN <dfain@wemaintain.io>
2022-08-08 12:18:26 +0200

    fix: do not bundle modern javascript

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import legacy from '@vitejs/plugin-legacy'
+import { load } from 'cheerio'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     legacy({ targets: 'Chrome 53', }),
+
+    {
+      name: 'Remove modern bundles',
+      enforce: 'post',
+      transformIndexHtml: {
+        // enforce: 'post',
+        transform: (html, ctx) => {
+          const $ = load(html);
+          console.log("notice me wesh", html)
+          $("[type=module]").remove()
+
+          console.log($.html())
+          return {
+            html: $.html(),
+            tags: []
+          }
+        }
+      }
+    },
     svelte(),
   ],
   build: {
